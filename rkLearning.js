271b9b8af08ab50715bf48d5dd5ba8818565d1b9
Rishi Karri <rishikarri1@gmail.com>
2017-03-02 11:53:24 -0500

    multiple ledges and have player on the screen

--- rkLearning.js
+++ rkLearning.js
@@ -9,6 +9,7 @@ $( document ).ready(()=>{
 	    game.load.image('star', 'assets/star.png');
 	    game.load.spritesheet('dude', 'assets/dude.png', 32, 48);
 	}
+	var player, cursors
 
 	function create() {
 		// we need to bring in physics so that we can jump off of ledges
@@ -18,10 +19,14 @@ $( document ).ready(()=>{
 		game.add.sprite(0, 0, 'sky');
 		game.add.sprite(0, 0, 'star');
 
+
 		// create a platforms group that our ledges will live in 
 		platforms = game.add.group();
 		// enable bbody so that people can jump off of ledges 
 		platforms.enableBody = true; 
+
+		var ground = platforms.create(0, game.world.height - 20, 'ground')
+		ground.body.immovable = true;
 
 		var ledge0 = platforms.create(-120, 500, 'ground');
 	    ledge0.body.immovable = false;
@@ -39,10 +44,23 @@ $( document ).ready(()=>{
 	    // the last ledge you can actually stay on
 	    ledge4.body.immovable = true;
 
+	    player = game.add.sprite(32, game.world.height - 80, 'dude')
+	    // give the player physics
+	    game.physics.arcade.enable(player);
+	    // give the player gravity so that he falls out of the air 
+	    player.body.gravity.y = 400;
+	    player.body.collideWorldBounds = false;	
+
+	    player.animations.add('left', [0, 1, 2, 3], 10, true);
+    	player.animations.add('right', [5, 6, 7, 8], 10, true);
+	    cursors = game.input.keyboard.createCursorKeys();
+
+
 
 	}
 
 	function update(){
-
+		//you pass collide a player and a group - does something if there is a collission
+		var hitPlatform = game.physics.arcade.collide(player, platforms);
 	}
 })
